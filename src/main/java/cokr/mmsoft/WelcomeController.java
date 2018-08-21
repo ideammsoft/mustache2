d623f26f9b805a7d0093c92fa60a5d76c83add88
kslee <ideammsoft@github.com>
2018-08-21 15:29:53 +0900

    web jar 오류나서 추가

--- src/main/java/cokr/mmsoft/WelcomeController.java
+++ src/main/java/cokr/mmsoft/WelcomeController.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import cokr.mmsoft.domain.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class WelcomeController {
